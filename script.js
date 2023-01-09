31ad2da1c92e9a0585d5f421cc40d6002be338e9
Danish Shafi <danish.shafi@live.co.uk>
2023-01-09 00:36:10 +0000

    Successfully able to change the password option based on condition user has chosen

--- script.js
+++ script.js
@@ -104,14 +104,6 @@ function getPasswordOptions() {
   var special = confirm("Would you like special characters in your password?");
   var numbers = confirm("Would you like numbers in your password?");
 
-  // var userPasswordOpts = {
-  //   plength: passwordLength,
-  //   opt1: lowercase,
-  //   opt2: uppercase,
-  //   opt3: special,
-  //   opt4: numbers
-  // };
-
   var userPasswordOpts = [passwordLength, lowercase, uppercase, special, numbers]
 
   return userPasswordOpts;
@@ -122,80 +114,29 @@ function getRandom(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-// Somehow map password options to random numbers. If that password option is false, don't include in the array.
 // Function to generate password with user input
 function generatePassword() {
   var userPasswordChoice = getPasswordOptions();
   var passwordChoiceWithoutLen = userPasswordChoice.slice(1);
   var passwordArr = [];
 
-  if (passwordChoiceWithoutLen.includes(false)) {
-
-    // var removeFalseFromArr = passwordChoiceWithoutLen.filter(function(bool) {
-    //   return bool !== false;
-    // });
-
-
-
-  } else {
-      for (var i = 0; i < userPasswordChoice[0]; i++) {
+  for (var i = 0; i < userPasswordChoice[0]; i++) {
+    switch(passwordChoiceWithoutLen.includes(false)) {
+      case passwordChoiceWithoutLen[0] === true:
+        passwordArr.push(getRandom(allCharsArr[0]));
+      case passwordChoiceWithoutLen[1] === true:
+        passwordArr.push(getRandom(allCharsArr[1]));
+      case passwordChoiceWithoutLen[2] === true:
+        passwordArr.push(getRandom(allCharsArr[2]));
+      case passwordChoiceWithoutLen[3] === true:
+        passwordArr.push(getRandom(allCharsArr[3]));
+        break;
+      default:
         passwordArr.push(getRandom(allCharsArr.flat()));
-      }
     }
-  // console.log(passwordArr);
+  }
   return passwordArr.join('');
 }
-
-
-
-
-// function generatePassword() {
-
-  // var passwordArr = [];
-
-//   if (Object.values(userPasswordChoice)) {
-
-//   let chatSetArr = [];
-//   const charSetTypes = Object.keys(userPasswordChoice).filter(charSetType => userPasswordChoice[charSetType]);
-//   for (var i = 0; i < userPasswordChoice.passwordLength; i++) {
-//     chatSetArr.push(...charSetTypes);
-//   }
-//   chatSetArr = chatSetArr.slice(chatSetArr.length-userPasswordChoice.passwordLength);
-//   shuffleArray(chatSetArr);
-
-//   return chatSetArr.map(charSet => getRandom(charSets[charSet])).join('');
-//   }
-//   return '';
-// }
-
-
-
-  // if (Object.values(userPasswordChoice)) {
-  //   for (var i = 0; i < userPasswordChoice.plength; i++) {
-
-  //     var randomNum = Math.floor(Math.random() * 3) + 1;
-
-  //     switch(randomNum) {
-  //       case 1:
-  //         passwordArr.push(getRandom(lowerCasedCharacters));
-  //       break;
-  //       case 2:
-  //         passwordArr.push(getRandom(upperCasedCharacters));
-  //       break;
-  //       case 3:
-  //         passwordArr.push(getRandom(specialCharacters));
-  //       break;
-  //       case 4:
-  //         passwordArr.push(getRandom(numericCharacters));
-  //       default:
-  //         null;
-  //     }
-  //   }
-  //   console.log(passwordArr);
-  // }
-  // console.log(Object.values(userPasswordChoice));
-  // return passwordArr.join('');
-// }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
